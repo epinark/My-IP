7331733885b18d773e9b4d3109be493e4c64295e
epinark <122348695+epinark@users.noreply.github.com>
2023-10-19 10:12:18 +0200

    add ip

--- my-ip/src/components/Ip.jsx
+++ my-ip/src/components/Ip.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Ip() {
-  return <div>IP</div>;
+  const [ipData, setIpData] = useState();
+  const [ipError, setIpError] = useState();
+
+  const apiKey = import.meta.env.VITE_IPIFY_KEY;
+
+  const url = `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(url);
+        if (!res.ok)
+          throw new Error(`Request failed ${res.status} ${res.statusText}`);
+        const data = await res.json();
+        setIpData(data);
+      } catch (error) {
+        setIpError(error);
+      }
+    }
+    fetchData();
+  }, [url]);
+  return <div>{ipData && ipData.ip}</div>;
 }
 
 export default Ip;
