7ced77e843932c59e6b55ba6bdb1f6e89c210920
epinark <122348695+epinark@users.noreply.github.com>
2023-11-10 17:43:58 +0100

    make main container with position absolute for responsive design

--- my-ip/src/components/Time.jsx
+++ my-ip/src/components/Time.jsx
@@ -15,16 +15,16 @@ function Time() {
   const date = currentTime.toLocaleString(DateTime.DATE_SHORT);
   const time = currentTime.toLocaleString(DateTime.TIME_24_SIMPLE);
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-wrap items-center">
+    <>
+      <div className="flex flex-row items-center p-4">
         <span className="material-icons">calendar_today</span>
-        <span className="pl-1">Today is {date}</span>
+        <span className="pl-2">Today is {date}</span>
       </div>
       <div className="flex flex-row items-center p-4">
         <span class="material-icons">schedule</span>
-        <span className="pl-1">Your local time is: {time}</span>
+        <span className="pl-2">Your local time is: {time}</span>
       </div>
-    </div>
+    </>
   );
 }
 
